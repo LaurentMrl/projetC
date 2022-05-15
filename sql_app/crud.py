c05c09293c47d7b17781d92f7b65f2abc0549aa1
laurent maréchal <laurent.mrl@outlook.com>
2022-05-15 16:25:24 +0200

    changes imports

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+import models, schemas
 
 
 def get_spPosQuotDep(db: Session, spPosQuotDep_id: int):
--- sql_app/main.py
+++ sql_app/main.py
@@ -1,8 +1,8 @@
 from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 
-from sql_app import crud, models, schemas
-from sql_app.database import SessionLocal, engine
+import crud, models, schemas
+from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
